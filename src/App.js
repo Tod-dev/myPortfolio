b87c4e87be264c29809ff1d7072861f08ebd939e
Marco Todaro <marcoposta59@gmail.com>
2020-08-19 11:51:05 +0200

    my portfolio is currently in development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="row">
+        <img src={logo} alt="That's my logo" />
+        <p className="App-description">
+          <span className="text">
+            My portfolio is currently under development
+          </span>
+          <span role="img" aria-label="work in progress" className="wave">
+            ⚙️
+          </span>
         </p>
-        <p>Deploy React on Netlify</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
