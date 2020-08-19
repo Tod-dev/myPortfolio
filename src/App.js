1f929fa08da6af80dcb20110c307fe11ffb7d31f
Marco Todaro <marcoposta59@gmail.com>
2020-08-19 14:16:02 +0200

    done but without react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React from "react";
 import "./App.css";
 
 import logo from "./images/logo.png";
-
 function App() {
   return (
     <div className="App">
-      <div className="row">
-        <img src={logo} alt="That's my logo" />
-        <p className="App-description">
-          <span className="text">
-            My portfolio is currently under development
-          </span>
-          <span role="img" aria-label="work in progress" className="wave">
-            ⚙️
-          </span>
-        </p>
-      </div>
+      <img src={logo} alt="That's my logo" className="responsiveImg" />
+      <p className="App-description">
+        My portfolio is currently under development
+        <span role="img" aria-label="work in progress" className="wave">
+          ⚙️
+        </span>
+      </p>
     </div>
   );
 }
